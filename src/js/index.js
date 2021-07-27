e7bd8b2dbb7d741309251578265ee3b41a20729a
danil fateev <rfney234@gmail.com>
2021-07-27 12:19:39 +0600

    finish work

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,56 @@
-$(document).ready(function(){
+$(document).ready(() => {
+    let w = $(window).width();
+    if( w < 650) { 
+      $('.accordion').attr('open', false);
+      $('.accordion-one').attr('open',true);
+    }
+    else { 
+      $('.accordion').attr('open', true);
+    }
     $('.slider').slick({
         dots: false,
         arrows: true,
         slidesToShow: 3,
         responsive: [
-            {
-              breakpoint: 800,
-              settings: {
-                slidesToShow: 2,
-                dots: true
-              }
-            },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              dots: true,
+              arrows: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              dots: true,
+              arrows: false
+            }
+          },
+          {
+            breakpoint: 520,
+            settings: {
+              slidesToShow: 1,
+              dots: true,
+              arrows: false
+            }
+          }
         ]
     });
 });
+
+$(window).resize( () => {
+  let w = $(window).width();
+  if( w < 650) { 
+    $('.accordion').attr('open', false);
+    $('.accordion-one').attr('open',true);
+  }
+  else { 
+    $('.accordion').attr('open', true);
+  }
+});
+
+
+
+
